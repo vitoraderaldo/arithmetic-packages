49b33a96d0d83a8c2630761f37c535f0c2cb1f3b
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-06-05 17:08:44 -0300

    fix: export more interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { EventDispatcher } from "./events/event.dispatcher";
+export { EventHandlerInterface } from "./events/event.handler.interface";
+export { EventInterface } from "./events/event.interface";
